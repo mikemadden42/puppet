ddaee53acdd37b068e77375f0d78c7a96cff84a4
Michael Madden <mikemadden42@gmail.com>
2014-01-09 10:28:02 -0600

    initial version of manifests/site.pp

---
+++ manifests/site.pp
@@ -0,0 +1,72 @@
+# http://www.puppetcookbook.com/posts/install-multiple-packages.html
+
+$packages = [
+  "bash",
+  "bzip2",
+  "cmake",
+  "coreutils",
+  "cpio",
+  "curl",
+  "dmidecode",
+  "gawk",
+  "gcc",
+  "gcc-c++",
+  "git",
+  "gzip",
+  "grep",
+  "iotop",
+  "ksh",
+  "less",
+  "lftp",
+  "lsof",
+  "lua",
+  "make",
+  "mlocate",
+  "nmap",
+  "java-1.6.0-openjdk",
+  "java-1.6.0-openjdk-devel", 
+  "net-tools",
+  "openssh",
+  "openssl",
+  "parted",
+  "perl",
+  "perltidy",
+  "puppet",
+  "procps",
+  "python",
+  "redhat-lsb",
+  "rpmlint",
+  "rpm-build",
+  "ruby",
+  "ruby-irb",
+  "ruby-rdoc",
+  "ruby-ri",
+  "rsync",
+  "screen",
+  "sed",
+  "strace",
+  "sudo",
+  "tar",
+  "tcpdump",
+  "vim-enhanced",
+  "wget",
+  "xz"
+]
+
+package { $packages:
+  ensure => 'installed',
+  #noop   => 'true'
+}
+
+$services = [
+  "crond",
+  "ntpd",
+  "rsyslog",
+  "sshd"
+]
+
+service { $services:
+  ensure => 'running',
+  enable => 'true',
+  #noop   => 'true'
+}
