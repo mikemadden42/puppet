d3d61760af73527b4184f1152fb874b7ecad8e7d
Michael Madden <mikemadden42@gmail.com>
2015-05-06 18:50:57 -0500

    Added manifest for CentOS / RHEL 7

---
+++ manifests/site_7.pp
@@ -0,0 +1,93 @@
+# http://www.puppetcookbook.com/posts/install-multiple-packages.html
+
+$packages = [
+  'bash',
+  'bzip2',
+  'clang-devel',
+  'cmake',
+  'coreutils',
+  'cpio',
+  'createrepo',
+  'curl',
+  'deltarpm',
+  'dmidecode',
+  'gawk',
+  'gcc',
+  'gcc-c++',
+  'git',
+  'glib2-devel',
+  'golang',
+  'golang-vet',
+  'golang-vim',
+  'gzip',
+  'grep',
+  'iotop',
+  'ksh',
+  'less',
+  'lftp',
+  'lsof',
+  'make',
+  'mlocate',
+  'nmap',
+  'ntp',
+  'java-1.7.0-openjdk',
+  'java-1.7.0-openjdk-devel',
+  'net-tools',
+  'openssh',
+  'openssl',
+  'parted',
+  'perl',
+  'perltidy',
+  'puppet',
+  'procps-ng',
+  'python',
+  'python-pip',
+  'redhat-lsb',
+  'rpmlint',
+  'rpm-build',
+  'ruby',
+  'ruby-irb',
+  'rsync',
+  'screen',
+  'sed',
+  'strace',
+  'sudo',
+  'tar',
+  'tcpdump',
+  'vim-enhanced',
+  'wget',
+  'xz',
+  'yum-cron',
+  'zsh'
+]
+
+package { $packages:
+  ensure => 'installed',
+  #noop   => true
+}
+
+$services = [
+  'crond',
+  'ntpd',
+  'rsyslog',
+  'sshd',
+  'yum-cron'
+]
+
+service { $services:
+  ensure => 'running',
+  enable => true,
+  #noop   => true
+}
+
+user { 'madden':
+  ensure     => 'present',
+  comment    => 'Michael Madden',
+  managehome => true,
+  #noop       => true,
+  shell      => '/bin/bash'
+}
+
+exec { 'yum_upgrade':
+  command => '/usr/bin/yum upgrade -y'
+}
