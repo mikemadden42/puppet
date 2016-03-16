3cf2e51c58cc9f8565ae0be9a58ca41c4b71ae4e
Michael Madden <mikemadden42@icloud.com>
2016-03-16 14:52:15 +0000

    Added support for debian jessie

---
+++ scripts/setup_puppet_jessie.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#gem install puppet-lint
+
+wget -c http://apt.puppetlabs.com/puppetlabs-release-jessie.deb 
+sudo dpkg -i puppetlabs-release-jessie.deb
+sudo apt-get update && sudo apt-get install puppet puppet-lint
+sudo rm -f puppetlabs-release-jessie.deb
