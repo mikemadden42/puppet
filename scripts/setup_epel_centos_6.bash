b0ac504fc5be987a023565d92d013fdf1513d0ba
Michael Madden <mikemadden42@icloud.com>
2016-12-20 16:02:20 -0600

    Clean up whitespace.

--- scripts/setup_epel_centos_6.bash
+++ scripts/setup_epel_centos_6.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-yum install -y http://mirror.sfo12.us.leaseweb.net/epel/6/x86_64/epel-release-6-8.noarch.rpm 
+yum install -y http://mirror.sfo12.us.leaseweb.net/epel/6/x86_64/epel-release-6-8.noarch.rpm
 yum update
--- scripts/setup_puppet_jessie.bash
+++ scripts/setup_puppet_jessie.bash
@@ -2,7 +2,7 @@
 
 #gem install puppet-lint
 
-wget -c http://apt.puppetlabs.com/puppetlabs-release-jessie.deb 
+wget -c http://apt.puppetlabs.com/puppetlabs-release-jessie.deb
 sudo dpkg -i puppetlabs-release-jessie.deb
 sudo apt-get update && sudo apt-get install puppet puppet-lint
 sudo rm -f puppetlabs-release-jessie.deb
