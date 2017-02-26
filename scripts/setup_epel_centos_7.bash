e1ab28d8876f2718349e21ab23e0c5c7b02b0849
Michael Madden <mikemadden42@icloud.com>
2017-02-26 16:29:17 -0600

    Made updates for centos 7.

--- scripts/setup_epel_centos_7.bash
+++ scripts/setup_epel_centos_7.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-yum install -y http://mirror.sfo12.us.leaseweb.net/epel/7/x86_64/e/epel-release-7-6.noarch.rpm
+yum install -y http://mirror.sfo12.us.leaseweb.net/epel/epel-release-latest-7.noarch.rpm 
 yum update
