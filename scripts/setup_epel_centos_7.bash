64cd0ab1df1790be7f6db80d70ed9a4f37739b68
Michael Madden <mikemadden42@icloud.com>
2017-07-29 08:22:36 -0500

    Added support for Debian Stretch.
    Tidy up scripts to add EPEL repo.

--- scripts/setup_epel_centos_7.bash
+++ scripts/setup_epel_centos_7.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-yum install -y http://mirror.sfo12.us.leaseweb.net/epel/epel-release-latest-7.noarch.rpm 
+yum install -y http://mirror.sfo12.us.leaseweb.net/epel/epel-release-latest-7.noarch.rpm
 yum update
