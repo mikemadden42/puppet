fed3aba4af7bc18b60244ed91a6687b7203ed787
Michael Madden <mikemadden42@icloud.com>
2017-12-21 10:38:11 -0600

    Inital setup for Ubuntu Bionic. Clean up scripts with shfmt.

--- scripts/setup_puppet_xenial.bash
+++ scripts/setup_puppet_xenial.bash
@@ -2,7 +2,7 @@
 
 apt-get update && apt-get dist-upgrade -y && apt-get clean
 apt install -y git vim wget
-wget -c http://apt.puppetlabs.com/puppetlabs-release-pc1-xenial.deb 
+wget -c http://apt.puppetlabs.com/puppetlabs-release-pc1-xenial.deb
 dpkg -i puppetlabs-release-pc1-xenial.deb && rm -f *.deb
 apt-get update
 apt-get install -y puppet-agent
